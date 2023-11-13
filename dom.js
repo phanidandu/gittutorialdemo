d48eae04b0fa49d137d0059942984f9760ae091b
phanidandu <phanid99@gmail.com>
2023-11-13 21:49:33 +0530

    task2

--- dom.js
+++ dom.js
@@ -2,10 +2,26 @@
 //console.log(document.domain);
 //console.log(document.all);
 //document.title="123"
-let title1=document.getElementById('header-title');
-console.log(title1);
+//let title1=document.getElementById('header-title');
+//console.log(title1);
 //title1.textContent='hello';
 //title1.innerText='bye';
 //title1.innerHTML='<h3>Hello</h3>'
 //console.log(title1);
-title1.style.borderBottom="solid 3px #000"
+//title1.style.borderBottom="solid 3px #000"
+let items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);  
+items[1].textContent='Hello 2';
+items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='yellow';
+
+for(i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor='#f4f4f4';
+}
+items[2].style.backgroundColor='green';
+for(i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}
